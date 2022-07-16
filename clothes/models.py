57672724b25d2e82f85f1312307b63e04a2f6353
dummy26 <dummyani26@gmail.com>
2022-07-16 12:15:45 +0530

    Add size and gender fields to Cloth

--- clothes/models.py
+++ clothes/models.py
@@ -1,12 +1,44 @@
-from pyexpat import model
 from django.db import models
 
 
 class Cloth(models.Model):
+    EXTRA_EXTRA_SMALL = 'XXS'
+    EXTRA_SMALL = 'XS'
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    EXTRA_LARGE = 'XL'
+    EXTRA_EXTRA_LARGE = 'XXL'
+
+    SIZE_CHOICES = [
+        (EXTRA_EXTRA_SMALL, EXTRA_EXTRA_SMALL),
+        (EXTRA_SMALL, EXTRA_SMALL),
+        (SMALL, SMALL),
+        (MEDIUM, MEDIUM),
+        (LARGE, LARGE),
+        (EXTRA_LARGE, EXTRA_LARGE),
+        (EXTRA_EXTRA_LARGE, EXTRA_EXTRA_LARGE),
+    ]
+
+    MEN = 'M'
+    WOMEN = 'W'
+    OTHERS = 'O'
+
+    GENDER_CHOICES = [
+        (MEN, 'Men'),
+        (WOMEN, 'Women'),
+        (OTHERS, 'Others'),
+    ]
+
     name = models.CharField(max_length=200)
     description = models.TextField()
     retail_price = models.PositiveIntegerField()
     sell_price = models.PositiveIntegerField()
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES)
+    gender = models.CharField(max_length=15, choices=GENDER_CHOICES)
 
     class Meta:
         verbose_name_plural = "clothes"
+
+    def __str__(self) -> str:
+        return self.name
--- clothes/serializers.py
+++ clothes/serializers.py
@@ -3,6 +3,10 @@ from .models import Cloth
 
 
 class ClothSerializer(serializers.ModelSerializer):
+
+    size = serializers.CharField(source='get_size_display')  # Make it read_only if POST should send 'M', 'W' or 'O'
+    gender = serializers.CharField(source='get_gender_display')
+
     class Meta:
         model = Cloth
-        fields = ('name', 'description', 'retail_price', 'sell_price')
+        fields = ('name', 'description', 'retail_price', 'sell_price', 'size', 'gender')
