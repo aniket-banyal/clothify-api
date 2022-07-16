acd810ad4b9f020ea93351639d20d836322d7c05
dummy26 <dummyani26@gmail.com>
2022-07-16 15:52:59 +0530

    Add color field to Cloth
    Added a fixed set of colors, so that clothes can be filtered on the basis of color

--- clothes/models.py
+++ clothes/models.py
@@ -30,12 +30,51 @@ class Cloth(models.Model):
         (OTHERS, 'Others'),
     ]
 
+    BEIGE = 'Beige'
+    BLACK = 'Black'
+    BLUE = 'Blue'
+    BROWN = 'Brown'
+    GREEN = 'Green'
+    GREY = 'Grey'
+    INDIGO = 'Indigo'
+    MAROON = 'Maroon'
+    MULTI = 'Multi'
+    NAVY = 'Navy'
+    NUDE = 'Nude'
+    ORANGE = 'Orange'
+    PINK = 'Pink'
+    PURPLE = 'Purple'
+    RED = 'Red'
+    WHITE = 'White'
+    YELLOW = 'Yellow'
+
+    COLOR_CHOICES = [
+        (BEIGE, BEIGE),
+        (BLACK, BLACK),
+        (BLUE, BLUE),
+        (BROWN, BROWN),
+        (GREEN, GREEN),
+        (GREY, GREY),
+        (INDIGO, INDIGO),
+        (MAROON, MAROON),
+        (MULTI, MULTI),
+        (NAVY, NAVY),
+        (NUDE, NUDE),
+        (ORANGE, ORANGE),
+        (PINK, PINK),
+        (PURPLE, PURPLE),
+        (RED, RED),
+        (WHITE, WHITE),
+        (YELLOW, YELLOW),
+    ]
+
     name = models.CharField(max_length=200)
     description = models.TextField()
     retail_price = models.PositiveIntegerField()
     sell_price = models.PositiveIntegerField()
     size = models.CharField(max_length=10, choices=SIZE_CHOICES)
     gender = models.CharField(max_length=15, choices=GENDER_CHOICES)
+    color = models.CharField(max_length=20, choices=COLOR_CHOICES)
 
     class Meta:
         verbose_name_plural = "clothes"
--- clothes/serializers.py
+++ clothes/serializers.py
@@ -6,7 +6,8 @@ class ClothSerializer(serializers.ModelSerializer):
 
     size = serializers.CharField(source='get_size_display')  # Make it read_only if POST should send 'M', 'W' or 'O'
     gender = serializers.CharField(source='get_gender_display')
+    color = serializers.CharField(source='get_color_display')
 
     class Meta:
         model = Cloth
-        fields = ('name', 'description', 'retail_price', 'sell_price', 'size', 'gender')
+        fields = ('name', 'description', 'retail_price', 'sell_price', 'size', 'gender', 'color')
