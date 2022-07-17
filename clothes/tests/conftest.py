53e5e263e0a7b9726325271317ba903f64391e89
dummy26 <dummyani26@gmail.com>
2022-07-17 16:39:32 +0530

    Add tests in clothes app

---
+++ clothes/tests/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+from ..models import Cloth
+
+User = get_user_model()
+
+
+@pytest.fixture
+def user_instance() -> User:
+    return User.objects.create(
+        email='a@a.com',
+        first_name='A',
+        last_name='B'
+    )
+
+
+@pytest.fixture
+def make_cloth():
+    def _make_cloth(name, description, retail_price, sell_price, size, gender, color, category, owner) -> Cloth:
+        return Cloth.objects.create(
+            name=name,
+            description=description,
+            retail_price=retail_price,
+            sell_price=sell_price,
+            size=size,
+            gender=gender,
+            color=color,
+            category=category,
+            owner=owner
+        )
+
+    return _make_cloth
+
+
+@pytest.fixture
+def cloth_instance(user_instance: User, make_cloth) -> Cloth:
+    return make_cloth(
+        name='Shirt',
+        description='Red shirt with checkmarks',
+        retail_price=1000,
+        sell_price=500,
+        size='M',
+        gender='M',
+        color='White',
+        category='Shirt',
+        owner=user_instance
+    )
---
+++ clothes/tests/test_cloth_model.py
@@ -0,0 +1,37 @@
+import pytest
+from clothes.models import Cloth
+
+
+@pytest.mark.django_db
+class TestClothModel:
+
+    @pytest.mark.parametrize(
+        "name, description, retail_price, sell_price, size, gender, color, category",
+        [
+            ('Shirt', 'Red shirt with checkmarks', 1000, 500, 'M', 'M', 'White', 'Shirt'),
+            ('Kurta', 'Casual Kurta in excellent condition', 800, 200, 'L', 'W', 'Yellow', 'Kurta')
+        ]
+    )
+    def test_cloth_model_insert_data(self, make_cloth, user_instance, name, description, retail_price, sell_price, size, gender, color, category):
+        owner = user_instance
+        cloth: Cloth = make_cloth(
+            name=name,
+            description=description,
+            retail_price=retail_price,
+            sell_price=sell_price,
+            size=size,
+            gender=gender,
+            color=color,
+            category=category,
+            owner=owner
+        )
+
+        assert cloth.name == name
+        assert cloth.description == description
+        assert cloth.retail_price == retail_price
+        assert cloth.sell_price == sell_price
+        assert cloth.size == size
+        assert cloth.gender == gender
+        assert cloth.color == color
+        assert cloth.category == category
+        assert cloth.owner == owner
