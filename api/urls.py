16c1cb0867dd618517f93731d6947d1e3d135989
dummy26 <dummyani26@gmail.com>
2022-07-18 17:45:49 +0530

    Add token authentication using django-rest-knox
    Closes #3.
    Also, make MEN_CATEGORY_CHOICES and WOMEN_CATEGORY_CHOICES lists, instead of set, cuz in sets order is diff every time which changes choice of category field and new migration is created.

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path('clothes/', include('clothes.urls')),
     path('users/', include('users.urls')),
+    path('auth/', include('knox.urls'))
 ]
