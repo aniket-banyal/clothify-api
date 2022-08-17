ec4b35b17b8fa236a7a1fba4f1fda388b45d5c16
dummy26 <dummyani26@gmail.com>
2022-08-17 14:35:04 +0530

    Add CartItem model
    Closes #13
    Closes #12

--- clothes/admin.py
+++ clothes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Cart, Category, Cloth, Image
+from .models import Cart, CartItem, Category, Cloth, Image
 
 admin.site.register(Cloth)
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Cart)
+admin.site.register(CartItem)
--- clothes/models.py
+++ clothes/models.py
@@ -206,5 +206,19 @@ class Image(models.Model):
 class Cart(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
+    def get_items(self):
+        return self.cartitem_set.all()
+
+    def get_total(self):
+        return sum(item.cloth.sell_price for item in self.get_items())
+
     def __str__(self) -> str:
         return f"Cart {self.user}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cloth = models.ForeignKey(Cloth, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.cart} {self.cloth}"
